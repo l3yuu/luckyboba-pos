ddec0ef95aab049e456d303aa7b689355bce6983
John Luigi Avila <avilajohnluigi@gmail.com>
2026-02-18 13:24:15 +0800

    add: responsive in tablet and phone on branch manager

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,6 +3,7 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import SalesOrder from './pages/SalesOrder'; // Import from 'pages' based on your screenshot
 import SuperAdminDashboard from './pages/SuperAdminDashboard';
+import BranchManagerDashboard from './pages/BranchManagerDashboard';
 import Calendar from './pages/Calendar';
 import { ProtectedRoute } from './components/ProtectedRoute';
 
@@ -22,6 +23,10 @@ export const router = createBrowserRouter([
       {
         path: '/super-admin',
         element: <SuperAdminDashboard />,
+      },
+      {
+        path: '/branch-manager',
+        element: <BranchManagerDashboard />,
       },
       {
         path: '/calendar',
