1c538a223b30d32ef7b66c65797e415f0dd1a006
Escaa007 <jimenezchristianjohngran07.com>
2026-02-23 16:09:55 +0700

    feat: with menu

---
+++ frontend/src/components/Menu/LuckyCard.tsx
@@ -0,0 +1,8 @@
+
+import type { ItemData } from './AddOns';
+
+export const LuckyCardList: ItemData[] = [
+  { name: "LUCKY CARD", price: 150.00 },
+  { name: "LUCKY CARD CHRISTMAS EDITION", price: 150.00 },
+  
+];
