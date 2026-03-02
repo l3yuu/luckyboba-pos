9aa2535a9551f69d37ceaa5562902b08e4a411e8
Chaeyoungnie <ernestdenila@gmail.com>
2026-03-02 15:16:33 +0800

    Reverb

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { CacheProvider } from './GlobalCache.tsx';
 import { AuthProvider } from './context/AuthContext';
 
 createRoot(document.getElementById('root')!).render(
